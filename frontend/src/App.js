48e57be42a41c42001f523442970cdd87680fa55
valeryfun <valeryfun@users.noreply.github.com>
2021-01-06 17:29:29 +0800

    shipping + payment screen & functionality frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
 	return (
@@ -21,6 +24,9 @@ const App = () => {
 					<Route path='/register' component={RegisterScreen} />
 					<Route path='/product/:id' component={ProductScreen} />
 					<Route path='/cart/:id?' component={CartScreen} />
+					<Route path='/shipping' component={ShippingScreen}/>
+					<Route path='/payment' component={PaymentScreen}/>
+					<Route path='/placeorder' component={PlaceOrderScreen}/>
 					<Route path='/' component={HomeScreen} exact />
 				</Container>
 			</main>
